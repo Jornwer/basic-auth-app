7949b32c840526a9bb3e1a56aabaffd96cf7afdb
Jornwer <betanovdmitry946@gmail.com>
2021-04-09 18:09:45 +0300

    Initial commit

---
+++ src/main/java/com/jornwer/basicauthapp/BasicauthappApplication.java
@@ -0,0 +1,13 @@
+package com.jornwer.basicauthapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicauthappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicauthappApplication.class, args);
+	}
+
+}
